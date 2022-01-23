26789c443e2e8255bfd950eb63b2be9587841668
Timothy Hull <timothyhull@gmail.com>
2022-01-23 08:10:43 +0000

    Add test_twitter_api_auth and test_get_tweets functions

--- tests/test_tweeter.py
+++ tests/test_tweeter.py
@@ -6,5 +6,46 @@
 # Imports - Third-Party
 
 # Imports - Local
+from app.tweeter import (
+    twitter_api_auth,  # get_tweets
+)
 
 # Constants
+
+
+def test_twitter_auth() -> None:
+    """ Test the twitter_auth function.
+
+        Args:
+            None.
+
+        Returns:
+            None
+    """
+
+    # Call the twitter_auth function
+    api = twitter_api_auth()
+
+    # Assert credential values are set
+    assert api.auth.consumer_key
+    assert api.auth.consumer_secret
+    assert api.auth.access_token
+    assert api.auth.access_token_secret
+
+    return None
+
+
+def test_get_tweets() -> None:
+    """ Test the get_tweets function.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+    """
+
+    # Call the get_tweets function
+    # get_tweets()
+
+    return None
