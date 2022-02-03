4fdb2ccbe7b3aa068dabd08d427e8718a291cd09
Timothy Hull <timothyhull@gmail.com>
2022-02-03 05:11:55 +0000

    Initial commit

---
+++ app/__dev__/sqlalchemy_tutorial.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+""" SQLAlchemy Object Relational Tutorial (1.x API).
+
+    Code to support the SQLAlchemy tutorial at:
+    https://docs.sqlalchemy.org/en/14/orm/tutorial.html
+"""
+
+# Imports - Python Standard Library
+
+# Imports - Third-Party
+import sqlalchemy
+
+# Imports - Local
+
+# Constants
+
+
+def sqlalchemy_version() -> str:
+    """ Display the SQLAlchemy version.
+
+        Args:
+            None.
+
+        Returns:
+            version (str):
+                SQLAlchemy version.
+    """
+
+    # Create a version display string for formatting
+    version_str = (
+        f'SQLAlchemy Version {sqlalchemy.__version__}'
+    )
+
+    # Create a header string
+    header_str = f'{"*" * len(version_str)}'
+
+    # Create a formatted header string
+    version = (
+        f'\n{header_str}\n'
+        f'*{version_str}*\n'
+        f'{header_str}\n'
+    )
+
+    return version
+
+
+def main() -> None:
+    """ Main program.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+    """
+
+    # Display SQLAlchemy version
+    sqlalchemy_version()
+
+
+if __name__ == '__main__':
+    main()
