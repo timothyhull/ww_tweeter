acefa0fefe98261eed401b7faa008c6e8a8600a1
Timothy Hull <timothyhull@gmail.com>
2022-02-05 05:34:47 +0000

    Add User class, create_db_engine function, get_user_schema function

--- app/__dev__/sqlalchemy_tutorial.py
+++ app/__dev__/sqlalchemy_tutorial.py
@@ -6,17 +6,60 @@
 """
 
 # Imports - Python Standard Library
+from os import getenv
 
 # Imports - Third-Party
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import declarative_base
 import sqlalchemy
 
 # Imports - Local
 
 # Constants
+BASE = declarative_base()
+DB_LOGGING = True
+DB_URL = getenv('DB_TEST_URL')
+
+
+# Class objects
+class User(BASE):
+    """ Class mapping to the users table.
+
+        References:
+            https://docs.sqlalchemy.org/en/14/orm/tutorial.html#declare-a-mapping
+
+        Args:
+            BASE (sqlalchemy.orm.decl_api.DeclarativeMeta)
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(
+        type_=Integer,
+        primary_key=True
+    )
+    name = Column(type_=String)
+    fullname = Column(type_=String)
+    nickname = Column(type_=String)
+
+    def __repr__(self) -> str:
+        repr_string = (
+            f'<User('f'name={self.name}, '
+            f'fullname={self.fullname}, '
+            f'nickname={self.fullname})>'
+        )
+
+        return repr_string
 
 
 def sqlalchemy_version() -> str:
     """ Display the SQLAlchemy version.
+
+        Reference:
+            https://docs.sqlalchemy.org/en/14/orm/tutorial.html#version-check
 
         Args:
             None.
@@ -37,11 +80,63 @@ def sqlalchemy_version() -> str:
     # Create a formatted header string
     version = (
         f'\n{header_str}\n'
-        f'* {version_str}* \n'
+        f'* {version_str} * \n'
         f'{header_str}\n'
     )
 
     return version
+
+
+def create_db_engine() -> Engine:
+    """ Create SQLAlchemy Engine object.
+
+        Creates core database interface through the PostgreSQL dialect.
+
+        References:
+            https://docs.sqlalchemy.org/en/14/orm/tutorial.html#connecting
+            https://docs.sqlalchemy.org/en/14/glossary.html#term-dialect
+            https://docs.sqlalchemy.org/en/14/glossary.html#term-DBAPI
+            https://docs.sqlalchemy.org/en/14/core/engines.html#postgresql
+
+        Args:
+            None.
+
+        Returns:
+            engine (sqlalchemy.engine.Engine):
+                SQLAlchemy Engine object.
+    """
+
+    # Create an sqlalchemy.engine.Engine object
+    engine = create_engine(
+        DB_URL,
+        echo=DB_LOGGING
+    )
+
+    return engine
+
+
+def get_user_schema() -> None:
+    """ Get an instance the User class schema.
+
+        The User class construction takes place via the Declaritive
+        system.  The Declarative system creates a Table object for the
+        users table.
+
+        References:
+            https://docs.sqlalchemy.org/en/14/orm/tutorial.html#create-a-schema
+
+        Args:
+            None.
+
+        Returns:
+            user (User):
+                Instance of the User class
+    """
+
+    # Display the user table schema object
+    user = User.__table__._schema_item_copy
+
+    return user
 
 
 def main() -> None:
@@ -56,6 +151,19 @@ def main() -> None:
 
     # Display SQLAlchemy version
     print(sqlalchemy_version())
+
+    # Create SQLAlchemy engine
+    engine = create_db_engine()
+
+    # Display engine object
+    print(f'Database Engine: "{engine}"\n')
+
+    # Display the user table schema
+    user = get_user_schema()
+    print(
+        f'Users table schema: \n'
+        f'\t{user}\n'
+    )
 
 
 if __name__ == '__main__':
