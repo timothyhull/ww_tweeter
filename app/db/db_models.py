2679bb49eb4210378c9ba96b4284937174922eb9
Timothy Hull <timothyhull@gmail.com>
2022-02-13 02:07:11 +0000

    Created database table models

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -1,10 +1,94 @@
 #!/usr/bin/env python3
-""" TODO """
+""" SQLAlchemy Models for db.py """
 
 # Imports - Python Standard Library
+from sqlalchemy import (
+    Column, DateTime, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
 
 # Imports - Third-Party
 
 # Imports - Local
 
 # Constants
+BASE = declarative_base()
+
+
+class Hashtag(BASE):
+    """ Create table for hashtags and hitcounts.
+
+        Args:
+            BASE (sqlalchemy.ext.declarative.declarative_base)
+    """
+
+    # Assign table name
+    __tablename__ = 'hashtags'
+
+    # Assign table columns
+    id = Column(
+        type_=Integer,
+        primary_key=True
+    )
+    name = Column(String(20))
+    count = Column(Integer)
+
+    # Create repr function
+    def __repr__(self):
+        """ Function that returns the hashtag name and count.
+
+            Args:
+                None.
+
+            Returns:
+                repr_string (str):
+                    Returns a string with the hashtag and hashtag
+                    count.
+        """
+
+        repr_string = (
+            f'<Hashtag(name={self.name}, count={self.count})>'
+        )
+
+        return repr_string
+
+
+class TweetData(BASE):
+    """ Create table for hashtags and hitcounts.
+
+        Args:
+            BASE (sqlalchemy.ext.declarative.declarative_base)
+    """
+
+    # Assign table name
+    __tablename__ = 'tweets'
+
+    # Assign table columns
+    id = Column(
+        type_=Integer,
+        primary_key=True
+    )
+    tweet_id = Column(String(22))
+    tweet_text = Column(String(300))
+    created = Column(DateTime)
+    likes = Column(Integer)
+    retweets = Column(Integer)
+
+    # Create repr function
+    def __repr__(self):
+        """ Function that returns the tweet ID and text.
+
+            Args:
+                None.
+
+            Returns:
+                repr_string (str):
+                    Returns a string with the tweet ID and tweet
+                    text.
+        """
+
+        repr_string = (
+            f'<Tweet(tweet_id={self.tweet_id}), tweet_text={self.tweet_text}>'
+        )
+
+        return repr_string
