1d69c4287a76e145c3437eda604f116330fae285
Timothy Hull <timothyhull@gmail.com>
2022-02-13 02:06:52 +0000

    Build initial test functions for classes in db_models.py

--- tests/test_db_models.py
+++ tests/test_db_models.py
@@ -6,7 +6,41 @@
 # Imports - Third-Party
 
 # Imports - Local
+from app.db.db_models import (
+    Hashtag, TweetData
+)
 
 # Constants
 
+
 # Test functions
+def test_instantiate_hashtag() -> None:
+    """ Create instance of the Hashtag class.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+    """
+
+    hashtag_instance = Hashtag()
+    assert hashtag_instance.__tablename__ == 'hashtags'
+
+    return None
+
+
+def test_instantiate_tweet_data() -> None:
+    """ Create instance of the TweetData class.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+    """
+
+    tweet_data_instance = TweetData()
+    assert tweet_data_instance.__tablename__ == 'tweets'
+
+    return None
