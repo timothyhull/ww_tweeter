954536248afc765e3f24ab43051bc9348a03af3c
Timothy Hull <timothyhull@gmail.com>
2022-02-14 06:20:04 +0000

    Create framework

--- app/db/db.py
+++ app/db/db.py
@@ -2,9 +2,56 @@
 """ TODO """
 
 # Imports - Python Standard Library
+from os import getenv
+import re
 
 # Imports - Third-Party
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm import sessionmaker
 
 # Imports - Local
+from app.db.db_models import (
+    BASE, Hashtag, TweetData
+)
 
 # Constants
+VALID_HASHTAG = (r'^[a-z0-9]+$')
+
+
+# Functions
+def _create_session() -> None:
+    """ """
+
+    return None
+
+
+def truncate_tables() -> None:
+    """ """
+
+    return None
+
+
+def get_hashtags() -> None:
+    """ """
+
+    return None
+
+
+def add_hashtags() -> None:
+    """ """
+
+    return None
+
+
+def get_tweets() -> None:
+    """ """
+
+    return None
+
+
+def add_tweets() -> None:
+    """ """
+
+    return None
--- tests/test_db.py
+++ tests/test_db.py
@@ -6,6 +6,10 @@
 # Imports - Third-Party
 
 # Imports - Local
+from app.db.db import (
+    _create_session, truncate_tables, get_hashtags, add_hashtags,
+    get_tweets, add_tweets
+)
 
 # Constants
 
