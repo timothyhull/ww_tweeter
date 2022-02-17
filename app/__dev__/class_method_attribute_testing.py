39b2713ce984ebf3371fcd72e99e5b64edc29527
Timothy Hull <timothyhull@gmail.com>
2022-02-17 02:39:23 +0000

    Initial commit

---
+++ app/__dev__/class_method_attribute_testing.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Tests for adding attributes to class methods.
+
+    The intent is to return two objects:
+        test_method_1().name
+        test_method_1().url.test_method_2()
+"""
+
+
+# Define a url class for the test method
+class URL:
+
+    # Define test method #2
+    def test_method_2(self, url):
+
+        # Return a string of the url parameter
+        return str(url)
+
+
+# Class objects
+class Test():
+
+    # Define test method #1
+    def test_method(self):
+
+        # Return the test method as an object, not a function call
+        return self.test_method
+
+    # Add the name attribute to the test method
+    test_method.name = 'test_name'
+
+    # Add the url attribute to the test method, instantiating the URL class
+    test_method.url = URL()
