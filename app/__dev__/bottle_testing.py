858c1ec701ca70ee320628b67ca6f9808bdd4de3
Timothy Hull <timothyhull@gmail.com>
2022-03-09 23:06:30 +0000

    Update docstring

---
+++ app/__dev__/bottle_testing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Tests for the bottle web/WSGI server.
+
+    Documentation:
+        http://bottlepy.org/docs/dev/
+
+    PyPI:
+        https://pypi.org/project/bottle/
+
+    Installation:
+        pip install bottle
+"""
+
+# Imports - Python Standard Library
+
+# Imports - Third-Party
+
+# Imports - Local
