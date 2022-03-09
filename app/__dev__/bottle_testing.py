5e6e7aa0e106b9bd33897516a2685d6302f9c72f
Timothy Hull <timothyhull@gmail.com>
2022-03-09 23:37:01 +0000

    Add initial bottle test code

--- app/__dev__/bottle_testing.py
+++ app/__dev__/bottle_testing.py
@@ -14,5 +14,33 @@
 # Imports - Python Standard Library
 
 # Imports - Third-Party
+from bottle import route, run  # , template
 
 # Imports - Local
+
+
+# Hello world test function
+@route
+def hello_world() -> str:
+    """ Display a hello world string.
+
+        Args:
+            None.
+
+        Returns:
+            display_string (str):
+                String to display in a web browser.
+    """
+
+    # Set a string to display
+    display_string = 'Hello World!'
+
+    return display_string
+
+
+# Call the run function to start the web service
+run(
+    host='localhost',
+    port=8080,
+    debug=True
+)
